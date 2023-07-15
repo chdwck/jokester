7d9fa76e0b2e6d8a2be6184af09558e1d9fb7c4f
Chadwick <cplattkuhn@gmail.com>
2023-07-14 21:35:12 -0600

    feat: cache dalle prompts

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,6 +2,8 @@ import express, { type Request, type Response } from 'express';
 import path from 'path';
 import axios from 'axios';
 import 'dotenv/config';
+import HitpointCache from './HitpointCache';
+
 const app = express();
 
 app.use('/', express.static(path.join(__dirname, '../client')));
@@ -15,6 +17,9 @@ const openaiClient = axios.create({
 })
 
 const promptPrefix = "Turn the following joke into a safe for work and funny image prompt for Dall-E based on the nouns in the joke. Joke: ";
+const TWO_WEEKS_MS = 1000 * 60 * 60 * 24 * 7 * 2;
+
+const cache = new HitpointCache(10, TWO_WEEKS_MS);
 app.get('/api/v1/dalle-image', async (req: Request, res: Response) => {
   if (!req.query.joke) {
     res.status(400).send('Missing query param: joke');
@@ -22,26 +27,48 @@ app.get('/api/v1/dalle-image', async (req: Request, res: Response) => {
   }
 
   const joke = req.query.joke;
+  if (typeof joke !== 'string') {
+    res.status(400).send('Invalid Query Param: joke must be a string')
+    return;
+  }
+
   try {
-    const chatCompletion = await openaiClient.post('chat/completions', {
-      model: 'gpt-3.5-turbo',
-      messages: [{ role: "system", content: promptPrefix + joke }],
-    })
-    const dallePrompt = chatCompletion.data.choices[0].message.content
+    let cacheHit = true;
+    let dallePrompt = cache.getItem(joke);
+
+    if (!dallePrompt) {
+      cacheHit = false;
+      const chatCompletion = await openaiClient.post('chat/completions', {
+        model: 'gpt-3.5-turbo',
+        messages: [{ role: "system", content: promptPrefix + joke }],
+      });
+
+      const choices = chatCompletion?.data?.choices ?? [];
+      dallePrompt = choices[0]?.message?.content;
+      if (!dallePrompt) {
+        throw new Error("Chat completion failed. Failed to create DallE prompt");
+      }
+
+      cache.addItem(joke, dallePrompt);
+    }
     const dalleImageRes = await openaiClient.post('images/generations', {
       prompt: dallePrompt,
       size: '512x512',
       n: 1,
       response_format: 'url',
     });
+    if (cacheHit) {
+      res.set("x-cache-status", cacheHit ? "HIT" : "MISS")
+    }
     res.status(200).json(dalleImageRes.data.data[0]);
   } catch (e: any) {
     res.status(500).json({ error: e.message });
   }
+  cache.cleanCache();
 });
 
 app.get('*', (req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, './client/dist/index.html'));
+  res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
 app.listen(process.env.PORT || 3000, () => console.log(`Server running on port ${process.env.PORT || 3000}`));
